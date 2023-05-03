a8934df5ad12562c50a482eb8a1044d850a0119a
Kenneth Joss <kenneth@cloudscale.ch>
2023-05-03 15:51:36 +0200

    Add docs for LB pool member

--- plugins/modules/load_balancer_pool_member.py
+++ plugins/modules/load_balancer_pool_member.py
@@ -27,25 +27,145 @@ options:
     description:
       - State of the load balancer pool member.
     choices: [ present, absent ]
-    default: running
+    default: present
     type: str
   name:
-
-
+    description:
+      - Name of the load balancer pool member.
+      - Either I(name) or I(uuid) are required.
+    type: str
+  uuid:
+    description:
+      - UUID of the load balancer.
+      - Either I(name) or I(uuid) are required.
+    type: str
   load_balancer_pool:
     description:
       - UUID of the load balancer pool.
     type: str
-
-
+  enabled:
+    description:
+      - Pool member will not receive traffic if false. Default is true.
+    default: true
+    type: bool
+  protocol_port:
+    description:
+      - The port to which actual traffic is sent.
+    type: str
+  monitor_port:
+    description:
+      - The port to which health monitor checks are sent.
+      - If not specified, protocol_port will be used. Default is null.
+    default: null
+    type: str
+  address:
+    description:
+      - The IP address to which traffic is sent.
+    type: str
+  subnet:
+    description:
+      - The subnet of the address must be specified here.
+    type: str
+  tags:
+    description:
+      - Tags assosiated with the load balancer. Set this to C({}) to clear any tags.
+    type: dict
+extends_documentation_fragment: cloudscale_ch.cloud.api_parameters
 '''
 
 EXAMPLES = '''
+# Create a pool member for a load balancer pool
+- name: Create a load balancer pool member
+  cloudscale_ch.cloud.load_balancer_pool_member:
+    name: 'my-shiny-swimming-pool-member'
+    load_balancer_pool: '20a7eb11-3e17-4177-b46d-36e13b101d1c'
+    enabled: true
+    protocol_port: '8080'
+    monitor_port: '8081'
+    subnet: '70d282ab-2a01-4abb-ada5-34e56a5a7eee'
+    address: '172.16.0.100'
+    tags:
+      project: ansible-test
+      stage: production
+      sla: 24-7
+    api_token: xxxxxx
 
+# Get load balancer pool member facts by name
+- name: Get facts of a load balancer pool member by name
+  cloudscale_ch.cloud.load_balancer_pool_member:
+    name: 'my-shiny-swimming-pool-member'
+    api_token: xxxxxx
 '''
 
 RETURN = '''
-
+href:
+  description: API URL to get details about this load balancer
+  returned: success when not state == absent
+  type: str
+  sample: https://api.cloudscale.ch/v1/load-balancers/pools/20a7eb11-3e17-4177-b46d-36e13b101d1c/members/b9991773-857d-47f6-b20b-0a03709529a9
+uuid:
+  description: The unique identifier for this load balancer pool member
+  returned: success
+  type: str
+  sample: cfde831a-4e87-4a75-960f-89b0148aa2cc
+name:
+  description: The display name of the load balancer pool member
+  returned: success
+  type: str
+  sample: web-lb-pool
+enabled:
+  description: THe status of the load balancer pool member
+  returned: success
+  type: str
+  sample: true
+created_at:
+  description: The creation date and time of the load balancer pool member
+  returned: success when not state == absent
+  type: datetime
+  sample: 2023-02-07T15:32:02.308041Z
+pool:
+  description: The pool of the pool member
+  returned: success
+  type: dict
+  sample: {
+            "href": "https://api.cloudscale.ch/v1/load-balancers/pools/20a7eb11-3e17-4177-b46d-36e13b101d1c",
+            "uuid": "20a7eb11-3e17-4177-b46d-36e13b101d1c",
+            "name": "web-lb-pool"
+            }
+protocol_port:
+  description: The port to which actual traffic is sent
+  returned: success
+  type: str
+  sample: 8080
+monitor_port:
+  description: The port to which health monitor checks are sent
+  returned: success
+  type: str
+  sample: 8081
+address:
+  description: The IP address to which traffic is sent
+  returned: success
+  type: str
+  sample: 10.11.12.3
+subnet:
+  description: The subnet in a private network in which address is located
+  returned: success
+  type: dict
+  sample: {
+            "href": "https://api.cloudscale.ch/v1/subnets/70d282ab-2a01-4abb-ada5-34e56a5a7eee",
+            "uuid": "70d282ab-2a01-4abb-ada5-34e56a5a7eee",
+            "cidr": "10.11.12.0/24"
+            }
+monitor_status:
+  description: The status of the pool's health monitor check for this member
+  returned: success
+  type: str
+  sample: up
+tags:
+  description: Tags assosiated with the load balancer
+  returned: success
+  type: dict
+  sample: { 'project': 'my project' }
 '''
 
 from copy import deepcopy
